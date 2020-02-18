0af8b9d207fee510a17c16cee175b14821089eb1
Christoffer Plovmand Simonsen <christoffer@ambolt.io>
2020-02-18 14:34:15 +0100

    Clean up and unrecognized type detection

--- src/dxf_utils.py
+++ src/dxf_utils.py
@@ -19,31 +19,33 @@ class DxfReader:
             raise Exception('File given is not of DXF type (.dxf)')
         else:
             self.dxf_path = dxf_file_path
+            self.doc = ezdxf.readfile(self.dxf_path)
+            self.msp = self.doc.modelspace()
+            self.data = []
+            self.unrecognized_types = []
+
+    def is_hidden(self, entity):
+        layer_name = entity.dxf.layer
+        layer = self.doc.layers.get(layer_name)
+        if layer.is_on() and not layer.is_frozen():
+            return False
+        else:
+            return True
 
     def extract_data(self):
-        doc = ezdxf.readfile(self.dxf_path)
-        msp = doc.modelspace()
-
         # Would be cool to preallocate memory to the data list
-        self.data = []
 
-        for e in msp:
+        for e in self.msp:
             if e.dxftype() == 'LINE':
                 point1 = e.dxf.start
                 point2 = e.dxf.end
-                # Some layers are frozen which means they are hidden, so neglect those
-                layer_name = e.dxf.layer
-                layer = doc.layers.get(layer_name)
-                if layer.is_frozen() == False:
+                if not self.is_hidden(e):
                     # Neglect the z-value as expected to be 0
                     line = [point1[:2], point2[:2]]
                     self.data.append(line)
             elif e.dxftype() == 'LWPOLYLINE':
-                # Initialize list with size equal to number of points within 'LWPOLYLINE' layout
-                lwpolyline = []
-                layer_name = e.dxf.layer
-                layer = doc.layers.get(layer_name)
-                if layer.is_frozen() == False:
+                if not self.is_hidden(e):
+                    lwpolyline = []
                     with e.points() as points:
                         # points is a list of points with the format = (x, y, [start_width, [end_width, [bulge]]])
                         for point in points:
@@ -52,16 +54,25 @@ class DxfReader:
                     self.data.append(lwpolyline)
             elif e.dxftype() == 'POLYLINE':
                 # POLYLINE points are saved as a list of vertices
-                layer_name = e.dxf.layer
-                layer = doc.layers.get(layer_name)
-                if layer.is_frozen() == False:
+                if not self.is_hidden(e):
                     polyline = []
                     vertices = e.vertices
                     for vertex in vertices:
                         point = vertex.dxf.location[:2]
                         polyline.append(point)
                     self.data.append(polyline)
+            else:
+                if not self. unrecognized_types:
+                    self.unrecognized_types.append([e.dxftype(), 1])
+                else:
+                    if e.dxftype() in list(zip(*self.unrecognized_types))[0]:
+                        self.unrecognized_types[list(zip(*self.unrecognized_types))[0].index(e.dxftype())][1] += 1
+                    else:
+                        self.unrecognized_types.append([e.dxftype(), 1])
 
+        for unrecognized_type in self.unrecognized_types:
+            print("Type " + str(unrecognized_type[0]) + " not recognized. Nr. of instances: "
+                  + str(unrecognized_type[1]))
         return self.data
 
     def plot_data(self):
